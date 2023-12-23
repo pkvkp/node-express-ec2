5fc6278908dada850d77a6df72628541c97622e1
rakesh-softoboticcs <97211884+rakesh-softoboticcs@users.noreply.github.com>
2023-12-23 20:28:11 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express()
+
+app.get("/",(req,res)=>{
+    res.send("My Api is running on ec2 instances😊")
+})
+
+
+app.listen(5000,()=>console.log("Server is listening in port 5000"))
